68858d9f6482c7799e2dff319f32d85fb023aa1f
Proton921 <111508354+Proton921@users.noreply.github.com>
2023-05-04 00:40:48 +0800

    Re-build.

---
+++ src/main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, Tray, Menu } = require('electron')
+const path = require('path')
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        show: false,
+        resizable: false,
+        width: 800,
+        height: 600,
+        backgroundColor: '#161619',
+        icon: 'resources/images/icons/2.ico',
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeItergration:true
+        }
+    })
+    mainWindow.loadFile('index.html')
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+      })
+}
+function createTray() {
+    const tray = new Tray('resources/images/icons/2.ico')
+    tray.setToolTip('StarRail.Launcher.Plus')
+    tray.setTitle('StarRail.Launcher.Plus')
+
+    tray.on('right-click', () => {
+        const template = [
+            {
+                label: '退出',
+                click: () => {
+                    app.quit()
+                }
+            },
+        ]
+        const menuConfig = Menu.buildFromTemplate(template)
+        tray.popUpContextMenu(menuConfig)
+    })
+}
+app.on("ready", () => {
+    createWindow()
+    createTray()
+})
+app.on('window-all-closed', () => {
+    app.quit()
+})
